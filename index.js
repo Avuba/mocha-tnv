b4db428e8940ccd4236e49325decf6148b2a4c59
Katharina Irrgang <katharina.irrgang@googlemail.com>
2015-03-05 17:29:09 +0100

    match files

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ var path = require('path'),
   argv = require('optimist').argv,
   fs = require('fs'),
   lodash = require('lodash'),
-  runner = require('./lib/runner'),
-  finder = require('./lib/finder'),
+  lib = require('./lib'),
   __private__ = {};
 
 
 __private__.config = {
   processes: 2,
-  testFilePostfix: '.test',
+  testFilePostfix: /.test/gi,
   applicationPath: path.normalize(__dirname + '/../../'),
   testPath: path.normalize(__dirname + '/../../test'),
   utilsPath: path.normalize(__dirname + '/../../test/utils.js'),
@@ -22,19 +21,20 @@ __private__.config = {
 };
 
 
-// tnv --config= --query=*
-// tnv --config= --query=example
-// tnv --config= --f=1 --q=*
-// tnv --config= --query=*
-
-
 module.exports = function() {
   if (argv.config) {
     __private__.config = lodash.merge(__private__.config, JSON.parse(fs.readFileSync(argv.config, { encoding: 'utf-8' })));
   }
 
-  var files = finder({ dir: __private__.config.testPath });
-  runner.run({
+  var files = lib.match({
+    files: lib.finder({ dir: __private__.config.testPath }),
+    match: argv.query,
+    isTestFile: function(file) {
+      return file.match(__private__.config.testFilePostfix);
+    }
+  });
+
+  lib.runner.run({
     config: __private__.config,
     files: files
   });
