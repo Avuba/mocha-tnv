62f07e7ec9b341ab8af0a59f9a7653fff5b17678
kirrg001 <katharina.irrgang@googlemail.com>
2016-02-05 20:41:48 +0100

    extend testing

--- test/match.test.js
+++ test/match.test.js
@@ -54,4 +54,52 @@ describe('UNIT: match', function() {
 
     matched.length.should.eql(1);
   });
+
+
+  it('exclude 2', function() {
+    var root = path.normalize(__dirname + '/../');
+
+    var matched = lib.match({
+      applicationPath: root,
+      files: [
+        {
+          fileName: 'create.test.js',
+          path: root + '/test/integration/7-others/1-shutdown.test.js'
+        },
+        {
+          fileName: 'create.test.js',
+          path: root + '/test/integration/1-card/create.test.js'
+        },
+        {
+          fileName: 'create.test.js',
+          path: root + '/test/unit/middlewares/utils.test.js'
+        }
+      ],
+      exclude: '7'
+    });
+
+    matched.length.should.eql(2);
+  });
+
+
+  it('without exclude', function() {
+    var root = path.normalize(__dirname + '/../');
+
+    var matched = lib.match({
+      applicationPath: root,
+      files: [
+        {
+          fileName: 'create.test.js',
+          path: root + '/test/unit/card/create.test.js'
+        },
+        {
+          fileName: 'create.test.js',
+          path: root + '/test/unit/user/create.test.js'
+        }
+      ],
+      query: 'create'
+    });
+
+    matched.length.should.eql(2);
+  });
 });
